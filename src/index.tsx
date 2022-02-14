19b3d087f29200a61d0f8830852e6db63ea663d8
aditReza <lete.note3@gmail.com>
2022-02-14 10:09:34 +0700

    hello world

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../public/styles/main.css";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { disableReactDevTools } from "./config/disableReactDevTools";
+
+process.env.NODE_ENV === "production" && disableReactDevTools();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ webpack/webpack.common.js
@@ -0,0 +1,85 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const commonPaths = require("./paths");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+module.exports = {
+  entry: ["babel-polyfill", commonPaths.entryPath],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      // {
+      //   test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      //   type: "asset/resource",
+      //   generator: {
+      //     filename: `${commonPaths.fontsFolder}/[name].[ext]`,
+      //   },
+      // },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[hash].[name].[ext]",
+              outputPath: commonPaths.imagesFolder,
+              esModule: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: ["src", "node_modules"],
+    extensions: ["*", ".tsx", ".ts", ".js", ".jsx", ".css", ".scss"],
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      // new update props
+      // check https://github.com/jantimon/html-webpack-plugin#options
+      title: 'ecommerce-react-ts',
+      template: commonPaths.templatePath,
+      filename: 'index.html',
+      scriptLoading: "defer", // {'blocking'|'defer'|'module'}
+      inject: true,
+      minify: true,
+      hash: false, // set true if using serviceworker
+      cache: true,
+      meta: {
+        // 'Content-Security-Policy': { 'http-equiv': 'Content-Security-Policy', 'content': 'default-src https:' },
+        // 'set-cookie': { 'http-equiv': 'set-cookie', content: 'name=value; expires=date; path=url' },
+        "theme-color": "#ffffff",
+        "description": "ecommerce using : react typescript, redux, tailwind, jest - rtl",
+        "og:type": "website",
+        "og:site_name": "ecommerce-react-ts",
+        "og:url": "https://trusting-jepsen-7e7a9c.netlify.app",
+        "og:image": "https://lumiere-a.akamaihd.net/v1/images/og-dt-b_42666fa8.jpeg?region=0%2C0%2C400%2C400",
+      },
+      // 'base': {
+      //   'href': 'https://trusting-jepsen-7e7a9c.netlify.app/index.html',
+      //   'target': '_blank'
+      // }
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: commonPaths.favIco, to: commonPaths.imagesFolder },
+        { from: commonPaths.appleIcon, to: commonPaths.imagesFolder },
+      ],
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+  ],
+};
