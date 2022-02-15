d9a7d6fe0314c079bd664d0204ad51779b01da3d
aditReza <lete.note3@gmail.com>
2022-02-15 15:36:20 +0700

    add routes

---
+++ src/routes/index.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { render } from "../test-utils";
+
+describe("__ROUTES", () => {
+  it("", () => {
+
+  })
+})
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Routes: React.FC<{}> = () => {
+  return (
+    <div>
+      hello world !!
+    </div>
+  )
+}
+
+export default Routes;
