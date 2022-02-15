d1297b2c35816321b78f532a385f4b13e9c17dfe
aditReza <lete.note3@gmail.com>
2022-02-15 16:39:14 +0700

    add basic routes

---
+++ src/containers/dashboard/checkout/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardCheckout: React.FC<{}> = () => {
+  return (
+    <div>
+      DashboardCheckout !!
+    </div>
+  )
+}
+
+export default DashboardCheckout;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,28 @@
 import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from "../containers/home";
+import Login from "../containers/login";
+import Signup from "../containers/signup";
+import DashboardCheckout from "../containers/dashboard/checkout";
 
-const Routes: React.FC<{}> = () => {
+const AppRoutes: React.FC<{}> = () => {
   return (
-    <div>
-      hello world !!
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Hello, React Router!</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/checkout" element={<DashboardCheckout />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
-export default Routes;
+export default AppRoutes;
